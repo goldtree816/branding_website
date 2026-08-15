0c756bf69da7c071d765de00160a02c4f7c6d964
swornim11 <swornimmaharjan020@gmail.com>
2026-08-15 19:54:34 +0545

    update

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,5 +1,6 @@
 import "../styles/blog.css";
 import ProjectCard from "../components/projectcard";
+import Free_Down from "../components/free";
 import b1 from "../assets/b1.png";
 import b2 from "../assets/b2.png";
 import b3 from "../assets/b3.png";
@@ -8,6 +9,17 @@ import b5 from "../assets/b5.png";
 import b6 from "../assets/b6.png";
 import b7 from "../assets/b7.png";
 import b8 from "../assets/b8.png";
+import b9 from "../assets/b9.png";
+import b10 from "../assets/b10.png";
+import b11 from "../assets/b11.png";
+import b12 from "../assets/b12.png";
+import b13 from "../assets/b13.png";
+import b14 from "../assets/b14.png";
+import b15 from "../assets/b15.png";
+import b16 from "../assets/b16.png";
+import free1 from "../assets/free1.png";
+import free2 from "../assets/free2.png";
+import free3 from "../assets/free3.png";
 
 
 
@@ -113,9 +125,109 @@ function Blog() {
   image={b8}
   title="Guides"
   description="Small Business Website Design: What You Actually Need (And What You Don't)"
+
+
+/>
+
+<ProjectCard
+  image={b9}
+  title="Luxury-Branding-Tips"
+  description="What Is Brand Identity Design? (And Why It's More Than a Logo)"
+/>
+
+
+<ProjectCard
+  image={b10}
+  title="Guides, Luxury-Branding-Tips"
+  description="8 Powerful Colors for Your Brand Kit, And What Each One Communicates"
+/>
+
+
+<ProjectCard
+  image={b11}
+  title="Guides, Luxury-Website-Tips"
+  description="Website Design for Wellness Professionals: What You Need to Know"
+/>
+
+
+<ProjectCard
+  image={b12}
+  title="Luxury-Website-Tips"
+  description="5 Signs Your Website Is Costing You Clients (And How to Fix It)"
+/>
+
+
+<ProjectCard
+  image={b13}
+  title="Luxury-Website-Tips"
+  description="Leave Link-Tree Behind: how to Build Your Own Multi-Link Page on your website"
+/>
+
+
+<ProjectCard
+  image={b14}
+  title="Luxury-Branding-Tips"
+  description="Why Your Business Needs Multiple Logos"
+/>
+
+
+<ProjectCard
+  image={b15}
+  title="Luxury-Branding-Tips, Luxury-Website-Tips"
+  description="Why professional brand photography is essential to your business & website"
+/>
+
+
+<ProjectCard
+  image={b16}
+  title="Luxury-Branding-Tips"
+  description="Free Branding Checklist for Entrepreneurs, freelancers, and small businesses"
+
 />
 
 </section>
+<section className="business-resources">
+      <div className="resource-card">
+        <div className="resource-content">
+          <h1>BUSINESS RESOURCES</h1>
+
+          <p>
+            <strong>Starting a business is no walk in the park, we feel ya!</strong>{" "}
+            Check out a collection of tools and resources that we personally
+            use and recommend for everything biz. From design tools,
+            content creation, planning, organizing, learning, and even
+            legal protection.
+          </p>
+        </div>
+
+        <div className="resource-button">
+          <a href="#">CHECK THEM OUT</a>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className="free-container">
+
+    <Free_Down
+        image={free1}
+        title="FREE DOWNLOAD: SERVICE-BASED BUSINESS WEBSITE BLUEPRINT"
+        description="The free blueprint breaks down every page of your service website. Section by section, so you know exactly what belongs there and why."
+    />
+
+    <Free_Down
+        image={free2}
+        title="FREE DOWNLOAD: AUDIT YOUR WEBSITE GUIDE"
+        description="Free resource to help you evaluate and enhance the performance of your current website. By conducting a website audit, you can identify and address any shortcomings or gaps in your site."
+    />
+
+    <Free_Down
+        image={free3}
+        title="FREE DOWNLOAD: BRANDING CHECKLIST"
+        description="Whether you're struggling to gain clarity with your brand, completely starting from scratch, or if you're DIY and want to get it done right, this guide will help you understand what you need."
+    />
+
+</div>
+    </section>
     </>
   );
 }
