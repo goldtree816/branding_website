fd094ad668b0dfe2df0f6ce0d7fcf0d45b4ab56f
swornim11 <swornimmaharjan020@gmail.com>
2026-08-16 17:12:09 +0545

    swornim update

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -1,38 +1,11 @@
-<<<<<<< HEAD:src/pages/contact.jsx
-import "../styles/contact.css";
-import Form from '../components/form.jsx'
-=======
-import "../styles/resources.css";
->>>>>>> a20535127bd1c1f9c7325551943b6b3ff28cd43b:src/pages/resources.jsx
+import React from 'react'
 
-function Resources() {
-
-    return (
-
-        <section id="contact" className="contact">
-
-            <div className="content">
-          <h1>GET IN TOUCH</h1>
-          <h2><i>The next level of your business starts here</i></h2>
-        </div>
-    
-
-      <div>
-        <h1>PROJECT INQUIRY FORM</h1>
-        <p>
-          I CAN'T WAIT TO HEAR ABOUT YOUR BUSINESS GOALS AND THE VISION FOR
-          YOUR PROJECT.
-        </p>
-        <p>Upon submission, you will be able to book your free discovery call directly on my calendar.</p>
-      </div>
-    
-    <Form/>
-     </section>
-
-    
-
-    );
-
+const resources = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-export default Resources;
+export default resources
